12c11af26361348d3496c3f68b75f0cdf251975b
David Obregón Sánchez <nano2766@gmail.com>
2024-04-20 13:20:03 -0500

    rename files (#8)

--- packages/front-end/src/pages/Sensors/Sensors.tsx
+++ packages/front-end/src/pages/Sensors/Sensors.tsx
@@ -9,39 +9,43 @@ import {
 } from "../../components";
 import { SensorsList } from "../../types/sensors/SensorsFixture";
 
+const sxMap = {
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "flex-start",
+    height: "100%",
+    width: "100%",
+    px: "4rem",
+    py: "2rem",
+  },
+  row: {
+    display: "flex",
+    flexDirection: "row",
+    width: "100%",
+    rowGap: "3rem",
+    justifyContent: "space-between",
+  },
+  list: {
+    cursor: "pointer",
+  },
+};
+
 const Sensors = () => {
   const [showDrawer, setShowDrawer] = useState(false);
   const handleAdd = () => {
     setShowDrawer(!showDrawer);
   };
   return (
-    <Box
-      sx={{
-        px: "4rem",
-        py: "2rem",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "flex-start",
-        height: "100%",
-        width: "100%",
-      }}
-    >
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "row",
-          width: "100%",
-          rowGap: "3rem",
-          justifyContent: "space-between",
-        }}
-      >
+    <Box sx={sxMap.container}>
+      <Box sx={sxMap.row}>
         <RefreshButton
           onClick={() => {
             console.log("Refreshing...");
           }}
         ></RefreshButton>
 
-        <Link onClick={handleAdd} sx={{ cursor: "pointer" }}>
+        <Link onClick={handleAdd} sx={sxMap.list}>
           <AddIcon color="action" fontSize="large">
             {" "}
           </AddIcon>
