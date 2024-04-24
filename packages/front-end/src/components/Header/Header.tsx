4e089818ffb87bfc74027ba82dc8db9653da249a
David Obregón Sánchez <nano2766@gmail.com>
2024-04-23 21:43:54 -0500

    Login redirection (#10)
    * Add fake auth provider
    
    * Implement logging in logic
    
    * Implement log out logic
    
    * Change text in tooltip

--- packages/front-end/src/components/Header/Header.tsx
+++ packages/front-end/src/components/Header/Header.tsx
@@ -1,20 +1,33 @@
+import { useState } from "react";
 import {
   AppBar,
   Avatar,
+  Box,
+  Button,
   Container,
   IconButton,
   List,
   ListItem,
   ListItemButton,
   ListItemText,
+  Menu,
+  MenuItem,
   Stack,
   Toolbar,
   Tooltip,
   Typography,
 } from "@mui/material";
-import { Link } from "react-router-dom";
+import { Link, useFetcher } from "react-router-dom";
 
 export const Header = () => {
+  const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null);
+  const fetcher = useFetcher();
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
   return (
     <AppBar position="static">
       <Container maxWidth="xl">
@@ -40,11 +53,37 @@ export const Header = () => {
             </List>
           </Stack>
 
-          <Tooltip title="Open settings">
-            <IconButton sx={{ p: 0 }}>
-              <Avatar alt="Profile" />
-            </IconButton>
-          </Tooltip>
+          <Box>
+            <Tooltip title="Opciones de usuario">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt="Profile" />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: "45px" }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              <MenuItem onClick={handleCloseUserMenu}>
+                <fetcher.Form method="post" action="/logout">
+                  <Button type="submit" variant="text" sx={{ color: "black" }}>
+                    Cerrar sesión
+                  </Button>
+                </fetcher.Form>
+              </MenuItem>
+            </Menu>
+          </Box>
         </Toolbar>
       </Container>
     </AppBar>
--- packages/front-end/src/pages/Login/Login.tsx
+++ packages/front-end/src/pages/Login/Login.tsx
@@ -1,30 +1,23 @@
 import { useState } from "react";
 import { Box, Button, Grid, TextField, Typography } from "@mui/material";
-import { Link } from "react-router-dom";
+import { Form, Link, LoaderFunctionArgs, redirect, useActionData, useLocation, useNavigation } from "react-router-dom";
+import { fakeAuthProvider } from "../../providers";
 
 const LoginPage: React.FC<unknown> = () => {
-  const [userName, setUserName] = useState("");
-  const [password, setPassword] = useState("");
+  let location = useLocation();
+  let params = new URLSearchParams(location.search);
+  let from = params.get("from") || "/";
+
+  let navigation = useNavigation();
+  let isLoggingIn = navigation.formData?.get("username") != null;
+
+  let actionData = useActionData() as { error: string } | undefined;
 
   const handleLogin = () => {};
   return (
     <Box>
-      <Grid
-        container
-        direction="column"
-        justifyContent="center"
-        alignItems="center"
-        style={{ height: "100vh" }}
-      >
-        <Grid
-          item
-          xs={12}
-          sm={8}
-          md={6}
-          lg={4}
-          justifyContent="center"
-          alignItems="center"
-        >
+      <Grid container direction="column" justifyContent="center" alignItems="center" style={{ height: "100vh" }}>
+        <Grid item xs={12} sm={8} md={6} lg={4} justifyContent="center" alignItems="center">
           <Typography variant="h4" gutterBottom align="center">
             Smart City UNAL
           </Typography>
@@ -34,26 +27,14 @@ const LoginPage: React.FC<unknown> = () => {
             <Typography variant="h5" gutterBottom align="center">
               Ingresa tu usuario y contraseña
             </Typography>
-            <form onSubmit={handleLogin}>
+            <Form method="post" replace>
+              <input type="hidden" name="redirectTo" value={from} />
               <Grid container spacing={2}>
                 <Grid item xs={12}>
-                  <TextField
-                    fullWidth
-                    placeholder="correo@unal.edu.co"
-                    variant="outlined"
-                    value={userName}
-                    onChange={(e) => setUserName(e.target.value)}
-                  />
+                  <TextField name="username" fullWidth placeholder="correo@unal.edu.co" variant="outlined" />
                 </Grid>
                 <Grid item xs={12}>
-                  <TextField
-                    fullWidth
-                    placeholder="Contraseña"
-                    type="password"
-                    variant="outlined"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                  />
+                  <TextField name="password" fullWidth placeholder="Contraseña" type="password" variant="outlined" />
                 </Grid>
                 <Box
                   sx={{
@@ -67,21 +48,52 @@ const LoginPage: React.FC<unknown> = () => {
                   <Link to={""}>¿Olvidó la contraseña?</Link>
                 </Box>
                 <Grid item xs={12}>
-                  <Button
-                    type="submit"
-                    color="primary"
-                    variant="contained"
-                    fullWidth
-                  >
-                    Ingresar
+                  <Button type="submit" color="primary" variant="contained" fullWidth disabled={isLoggingIn}>
+                    {isLoggingIn ? "Ingresando..." : "Ingresar"}
                   </Button>
                 </Grid>
               </Grid>
-            </form>
+            </Form>
           </Box>
         </Grid>
       </Grid>
     </Box>
   );
 };
+
+export const loginAction = async ({ request }: LoaderFunctionArgs) => {
+  let formData = await request.formData();
+  let username = formData.get("username") as string | null;
+  let userPassword = formData.get("password") as string | null;
+
+  // Validate our form inputs and return validation errors via useActionData()
+  if (!username) {
+    return {
+      error: "Debes ingresar un nombre de usuario valido!",
+    };
+  }
+
+  // Sign in and redirect to the proper destination if successful.
+  try {
+    await fakeAuthProvider.signin(username);
+  } catch (error) {
+    // Unused as of now but this is how you would handle invalid
+    // username/password combinations - just like validating the inputs
+    // above
+    return {
+      error: "Intento de inicio de sesión no válido",
+    };
+  }
+
+  let redirectTo = formData.get("redirectTo") as string | null;
+  return redirect(redirectTo || "/");
+};
+
+export const loginLoader = async () => {
+  if (fakeAuthProvider.isAuthenticated) {
+    return redirect("/");
+  }
+  return null;
+};
+
 export default LoginPage;
