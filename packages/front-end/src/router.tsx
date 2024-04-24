2b899ccc78ec080720abc9996e74fd0cc9e5f865
David Obregon <nano2766@gmail.com>
2024-04-23 19:30:48 -0500

    Implement logging in logic

--- packages/front-end/src/router.tsx
+++ packages/front-end/src/router.tsx
@@ -1,14 +1,28 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, redirect } from "react-router-dom";
 import { PageLayout } from "./components";
 import Home from "./pages/Home/Home";
 import NotFound from "./pages/NotFound/NotFound";
 import Sensors from "./pages/Sensors/Sensors";
 import Alerts from "./pages/Alerts/Alerts";
-import LoginPage from "./pages/Login/Login";
+import LoginPage, { loginAction, loginLoader } from "./pages/Login/Login";
+import { fakeAuthProvider } from "./providers";
 
 const router = createBrowserRouter([
   {
     element: <PageLayout />,
+    loader({ request }) {
+      // If the user is not logged in and tries to access any route under this Layout, we redirect
+      // them to `/login` with a `from` parameter that allows login to redirect back
+      // to this page upon successful authentication
+      if (!fakeAuthProvider.isAuthenticated) {
+        let params = new URLSearchParams();
+        params.set("from", new URL(request.url).pathname);
+        return redirect("/login?" + params.toString());
+      }
+
+      // Our root route always provides the user, if logged in
+      return { user: fakeAuthProvider.username };
+    },
     children: [
       {
         path: "/",
@@ -27,6 +41,8 @@ const router = createBrowserRouter([
   },
   {
     path: "/login",
+    action: loginAction,
+    loader: loginLoader,
     element: <LoginPage />,
   },
 ]);
