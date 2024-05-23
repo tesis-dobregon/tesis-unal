507414476309a69599c38713c51be19ce38bee2c
David Obregon <david.obregon@contractor.jupiterone.com>
2024-05-22 22:06:12 -0500

    add backend package with deploy to kubernetes

---
+++ packages/backend/build.sh
@@ -0,0 +1,3 @@
+docker build --no-cache -t smart-city-unal . --platform="linux/amd64"
+docker tag smart-city-unal:latest us.gcr.io/smart-city-unal/smart-city-unal:0.2.0
+docker push us.gcr.io/smart-city-unal/smart-city-unal:0.2.0
